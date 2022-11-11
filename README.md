7697cb305238754fffbcbce415f112fd72d32579
TiyaZhan <tizhan@ucsd.edu>
2022-11-10 17:40:49 -0800

    Readme.md update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lab 7 - Starter
+
+Name: Tianya Zhan
+Partner: None
+
+[Link to website](https://tiyazhan.github.io/cse110_Lab7_Starter/)
--- sw.js
+++ sw.js
@@ -50,12 +50,8 @@ self.addEventListener('fetch', function (event) {
     // Respond with the image from the cache or from the network
     return cache.match(event.request).then((cachedResponse) => {
       return cachedResponse || fetch(event.request).then((fetchedResponse) => {
-        // Add the network response to the cache for future visits.
-        // Note: we need to make a copy of the response to save it in
-        // the cache and use the original as the request response.
         cache.put(event.request, fetchedResponse.clone());
 
-        // Return the network response
         return fetchedResponse;
       });
     });
